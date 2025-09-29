9aad12abbdf170b115591a306ad5a3fa37ae7fc1
Flo Kleiser <floriankleiser@gmail.com>
2025-09-29 22:39:04 +0200

    now all values are 0 upside down

--- src/main.cpp
+++ src/main.cpp
@@ -1,61 +1,65 @@
 #define ARDUINO_USB_CDC_ON_BOOT 1
 #include <M5Unified.h>
 
-void setup()
-{
-    auto cfg = M5.config();
-    cfg.serial_baudrate = 115200;
-    M5.begin(cfg);
+float gx = 0.0, gy = -1.0, gz = 0.0;
 
-    M5.Imu.begin();
+void setup() {
+  auto cfg = M5.config();
+  cfg.serial_baudrate = 115200;
+  M5.begin(cfg);
 
-    M5.Display.setTextSize(1.5);
-    M5.Display.println("IMU Data Reader");
-    M5.Display.println("Starting...");
+  M5.Imu.begin();
 
-    Serial.println("test");
+  M5.Display.setTextSize(1.5);
+  M5.Display.println("IMU Data Reader");
+  M5.Display.println("Starting...");
 
-    delay(1000);
+  Serial.println("test");
+
+  delay(1000);
 }
 
-void loop()
-{
-    float accX = 0.0F;
-    float accY = 0.0F;
-    float accZ = 0.0F;
+void loop() {
+  float accX = 0.0F;
+  float accY = 0.0F;
+  float accZ = 0.0F;
 
-    float gyroX = 0.0F;
-    float gyroY = 0.0F;
-    float gyroZ = 0.0F;
+  float gyroX = 0.0F;
+  float gyroY = 0.0F;
+  float gyroZ = 0.0F;
 
-    float pitch = 0.0F;
-    float roll = 0.0F;
-    float yaw = 0.0F;
+  float pitch = 0.0F;
+  float roll = 0.0F;
+  float yaw = 0.0F;
 
-    M5.Imu.getAccel(&accX, &accY, &accZ);
-    M5.Imu.getGyro(&gyroX, &gyroY, &gyroZ);
+  M5.Imu.getAccel(&accX, &accY, &accZ);
+  M5.Imu.getGyro(&gyroX, &gyroY, &gyroZ);
 
-    //nice screen
-    M5.Display.clear();
-    M5.Display.setCursor(0, 0);
-    M5.Display.printf("Accel:\n");
-    M5.Display.printf("X: %.2f\n", accX);
-    M5.Display.printf("Y: %.2f\n", accY);
-    M5.Display.printf("Z: %.2f\n", accZ);
+  // nice screen
+  M5.Display.clear();
+  M5.Display.setCursor(0, 0);
+  M5.Display.printf("Test:\n");
+  M5.Display.printf("X: %.2f\n", accX);
+  M5.Display.printf("Y: %.2f\n", accY);
+  M5.Display.printf("Z: %.2f\n", accZ);
 
-    M5.Display.printf("\nGyro:\n");
-    M5.Display.printf("X: %.2f\n", gyroX);
-    M5.Display.printf("Y: %.2f\n", gyroY);
-    M5.Display.printf("Z: %.2f\n", gyroZ);
+  M5.Display.printf("\nGyro:\n");
+  M5.Display.printf("X: %.2f\n", gyroX);
+  M5.Display.printf("Y: %.2f\n", gyroY);
+  M5.Display.printf("Z: %.2f\n", gyroZ);
 
+  float ax = accX - gx;
+  float ay = accY - gy;
+  float az = accZ - gz;
 
-    // Serial.printf("Accel: X=%.2f Y=%.2f Z=%.2f | \n", accX, accY, accZ);
-    //Y will be around 1 when upright
-    Serial.printf("%.2f, %.2f, %.2f \n", accX, accY, accZ);
+  // Serial.printf("Accel: X=%.2f Y=%.2f Z=%.2f | \n", accX, accY, accZ);
+  // Y will be around 1 when upright
+  // Serial.printf("%.2f, %.2f, %.2f \n", accX, -accY, accZ);
+  Serial.printf("%.2f, %.2f, %.2f\n", ax, ay, az);
 
-    //we dont really need gyro
-    // Serial.printf("Gyro: X=%.2f Y=%.2f Z=%.2f | \n", gyroX, gyroY, gyroZ);
+  // we dont really need gyro
+  //  Serial.printf("Gyro: X=%.2f Y=%.2f Z=%.2f | \n", gyroX, gyroY, gyroZ);
 
-    M5.update();
-    delay(100);
+  M5.update();
+  delay(100);
 }
